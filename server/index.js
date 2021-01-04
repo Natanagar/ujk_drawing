6fc00cdc708d8fe50b51b20c39b449ac2f8d7cba
ciemna_noc <agarkowa@gmail.com>
2021-01-04 19:08:38 +0100

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+
+const dotenv = require("dotenv");
+const users = require('./users.json');
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
+
+app.get('/api/users', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(users));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () =>
+    console.log('Example app listening on port ' + port + '!')
+);
